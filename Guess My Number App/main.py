93544825775da8615cf6d87221af2c8fa0b640cc
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2024-03-25 09:47:10 +0530

    Add comments for Guess my number

--- Guess My Number App/main.py
+++ Guess My Number App/main.py
@@ -4,9 +4,13 @@ print('Welcome to the Guess My Number App')
 usrName = input('\nEnter your name: ')
 print(f'Well {usrName}, I am thinking of a number between 1 and 20.')
 
+# Store a random number guessed by machine
 randomNum = random.randint(1,20)
 
+# To display after how many guesses user was able to guess the number
 count = 1
+
+# Allow the user to guess the number in 5 trys
 for _ in range(4):
     usrGuess = int(input('\nTake a guess: '))
     if usrGuess == randomNum:
